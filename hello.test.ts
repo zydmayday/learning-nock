9b4ed16e71d1fe78904e0f168b147262f700ec24
zyd <zydmayday@gmail.com>
2024-04-05 21:55:08 +0900

    nock different cases

--- hello.test.ts
+++ hello.test.ts
@@ -1,15 +1,36 @@
-import nock from 'nock';
-import axios from 'axios';
+import nock from "nock";
+import axios from "axios";
 
-const scope = nock('http://localhost:8080');
-scope.get('/api/user/1').reply(200, {'name': 'zyd'});
-
-describe('test nock', () => {
-    it('test nock get', async () => {
-        let res = await axios.get('http://localhost:8080/api/user/1');
-        expect(res.data).toEqual({'name': 'zyd'});
-        
-        res = await axios.get('http://localhost:8080/api/user/1');
-        expect(res.data).toEqual({'name': 'zyd'});
-    })
+const scope = nock("http://localhost:8080");
+beforeEach(() => {
+  nock.cleanAll(); // keep all tests isolated
 })
+
+describe("test nock", () => {
+  it("test nock get", async () => {
+    scope.get("/api/users/1").twice().reply(200, { name: "zyd" });
+    let res = await axios.get("http://localhost:8080/api/users/1");
+    expect(res.data).toEqual({ name: "zyd" });
+
+    res = await axios.get("http://localhost:8080/api/users/1");
+    expect(res.data).toEqual({ name: "zyd" });
+    scope.done();
+  });
+
+  it("test nock get users", async () => {
+    scope.get("/api/users").reply(200, [{ name: "zyd" }, {name: 'lh'}]);
+    let res = await axios.get("http://localhost:8080/api/users");
+    expect(res.data).toEqual([{ name: "zyd" }, {name: 'lh'}]);
+    scope.done();
+  });
+
+  it("test nock without done", async () => {
+    scope.get("/api/users").reply(200, [{ name: "zyd" }, {name: 'lh'}]);
+  });
+
+  it.skip("test nock use scope from previous test", async () => {
+    let res = await axios.get("http://localhost:8080/api/users");
+    expect(res.data).toEqual([{ name: "zyd" }, {name: 'lh'}]);
+    scope.done();
+  });
+});
