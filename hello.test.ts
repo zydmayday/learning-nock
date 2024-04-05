2e9f91fbd7de77f187cd27f4316fd983866bf5a1
Zhang Yidong <zydmayday@gmail.com>
2024-04-05 12:46:46 +0000

    first nock test

--- hello.test.ts
+++ hello.test.ts
@@ -1,5 +1,15 @@
-describe('test', () => {
-    it('test', () => {
-        expect(1).toEqual(1)
+import nock from 'nock';
+import axios from 'axios';
+
+const scope = nock('http://localhost:8080');
+scope.get('/api/user/1').reply(200, {'name': 'zyd'});
+
+describe('test nock', () => {
+    it('test nock get', async () => {
+        let res = await axios.get('http://localhost:8080/api/user/1');
+        expect(res.data).toEqual({'name': 'zyd'});
+        
+        res = await axios.get('http://localhost:8080/api/user/1');
+        expect(res.data).toEqual({'name': 'zyd'});
     })
 })
